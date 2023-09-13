e2000fccca2bcffadf678da070f7cc2d43578548
Kevin <staffordkevinjames@gmail.com>
2023-09-13 12:38:42 -0400

    completes functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+//Implement a function that returns an 'inner' function
+function wrapAdjective(addFlair = '*') {
+    return function (encouragingMessage = "special") {
+        return `You are ${addFlair}${encouragingMessage}${addFlair}!`
+    }
+}
